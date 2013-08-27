7921a2778178578902bd27787a4f444b6356d359
Todd Pickell <myappleguy@gmail.com>
2013-08-26 19:45:01 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularStarSurfer::Application.config.secret_key_base = 'cec0fa5dab988488eb61b71137126ecdfc82b8ae0f0eb18841e8de3f2204c65855b02bf391454a8b5262e29a1be503962f4f970fc54cfbbe9183dea01e330bc5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngularStarSurfer::Application.config.session_store :cookie_store, key: '_angular_star_surfer_session'
